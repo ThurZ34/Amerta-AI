ce46f3fbde615b390e39207d4e58b39702fb26e9
Roja Fadilah <rojafadilah0203@gmail.com>
2025-12-01 21:16:56 +0800

    fix: theme bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
-    
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
     <style>
